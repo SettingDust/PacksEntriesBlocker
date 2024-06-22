bbf5f4b4cb71bc732030a11c68a5a2f8f578fda7
SettingDust <settingdust@gmail.com>
2024-06-22 11:20:00 +0800

    chore: fix the source in manifest

--- build.gradle.kts
+++ build.gradle.kts
@@ -78,7 +78,7 @@ val metadata =
         "name" to name,
         "version" to version,
         "description" to description,
-        "source" to "https://github.com/SettingDust/DataDumper",
+        "source" to "https://github.com/SettingDust/PackEntriesBlocker",
         "minecraft" to "~1.21",
         "fabric_loader" to ">=0.15",
         "fabric_kotlin" to ">=1.11",
